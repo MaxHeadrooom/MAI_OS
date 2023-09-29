da5206764cf166b2f0307128cc5bc42d50ad94f2
Esmedlyaev Fedor <89976445+MaxHeadrooom@users.noreply.github.com>
2023-09-29 21:06:14 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,29 +7,36 @@
 
 using namespace std;
 
-mt19937 rng(std::chrono::steady_clock::now().time_since_epoch().count());
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 
-char* read_string(size_t& string_len) {
+char *read_string(size_t &string_len)
+{
     string_len = 0;
     size_t string_cap = 1;
-    char* string = new char[string_cap];
-    char c = std::cin.get();
-    if (c == '\n') c = std::cin.get();
-    if (c == EOF) return nullptr;
-    while (c != '\n') {
+    char *string = new char[string_cap];
+    char c = cin.get();
+    if (c == '\n')
+        c = cin.get();
+    if (c == EOF)
+        return nullptr;
+    while (c != '\n')
+    {
         string[string_len] = c;
         string_len++;
-        if (string_len >= string_cap) {
+        if (string_len >= string_cap)
+        {
             string_cap *= 2;
-            char* new_string = new char[string_cap];
-            for (size_t i = 0; i < string_len; i++) {
+            char *new_string = new char[string_cap];
+            for (size_t i = 0; i < string_len; i++)
+            {
                 new_string[i] = string[i];
             }
             delete[] string;
             string = new_string;
         }
         c = std::cin.get();
-        if (c == EOF) return nullptr;
+        if (c == EOF)
+            return nullptr;
     }
     string[string_len] = '\0';
     return string;
@@ -37,7 +44,6 @@ char* read_string(size_t& string_len) {
 
 int main()
 {
-    uniform_int_distribution<int> distribution(1, 100);
     string file_name_1;
     char symbol_1;
 
@@ -83,7 +89,7 @@ int main()
     {
         return -1;
     }
-    
+
     int fd_1[2]; // fd[0] - read; fd[1] - write
     // Creating pipe and checking that it was created
     if (pipe(fd_1) == -1)
@@ -103,9 +109,9 @@ int main()
     {
         return 2;
     }
-    else if (id_1 > 0)//in 1 process
+    else if (id_1 > 0) // in 1 process
     {
-        close(fd_1[0]);
+        // close(fd_1[0]);
 
         int id_2 = fork();
 
@@ -115,22 +121,15 @@ int main()
         }
         else if (id_2 > 0)
         {
-            close(fd_2[0]);
-            close(file_1);
-            close(file_2);
 
             size_t len = 0;
             char *stroka = read_string(len);
 
             char p = 's'; // start
-            if (stroka != nullptr)
-                cout << 5 << '\n';
-            else
-                cout << 6 << '\n';
+
             while (stroka != nullptr)
             {
-                
-                if (distribution(rng) <= 80)
+                if (rng() % 100 <= 80)
                 {
                     write(fd_1[1], &p, 1);
                     write(fd_1[1], &len, sizeof(len));
@@ -142,15 +141,17 @@ int main()
                     write(fd_2[1], &len, sizeof(len));
                     write(fd_2[1], stroka, sizeof(char) * len);
                 }
-                cout << "11";
                 stroka = read_string(len);
-                cout << stroka << "123213" << '\n';
-            }  
-            p = 'e';  // end
+            }
+            p = 'e'; // end
             write(fd_1[1], &p, 1);
             write(fd_2[1], &p, 1);
+            close(file_1);
+            close(file_2);
             close(fd_1[1]);
+            close(fd_1[0]);
             close(fd_2[1]);
+            close(fd_2[0]);
         }
         else
         {
@@ -158,35 +159,34 @@ int main()
             close(fd_1[1]);
             close(fd_1[0]);
             close(file_1);
-            if (dup2(file_2, STDOUT_FILENO) == -1) 
+            if (dup2(file_2, STDOUT_FILENO) == -1)
             {
                 return -1;
             }
             close(file_2);
 
-            if (dup2(fd_2[0], STDIN_FILENO) == -1) 
+            if (dup2(fd_2[0], STDIN_FILENO) == -1)
             {
                 return -1;
             }
             close(fd_2[0]);
 
-
             execl("./son.out", NULL);
             return 0;
         }
     }
-    else 
+    else
     {
         close(fd_1[1]);
         close(fd_2[0]);
         close(fd_2[1]);
         close(file_2);
-        if (dup2(file_1, STDOUT_FILENO) == -1) 
+        if (dup2(file_1, STDOUT_FILENO) == -1)
         {
             return -1;
         }
         close(file_1);
-        if (dup2(fd_1[0], STDIN_FILENO) == -1) 
+        if (dup2(fd_1[0], STDIN_FILENO) == -1)
         {
             return -1;
         }
